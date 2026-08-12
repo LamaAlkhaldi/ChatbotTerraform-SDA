6821b00fc5d79fc34dc2c60f4736ba711246c73c
LamaAlkhaldi <Alkhaldi.lama@hotmail.com>
2026-08-12 07:54:26 +0300

    Add files via upload

---
+++ terraform-project/main.tf
@@ -0,0 +1,47 @@
+resource "azurerm_resource_group" "myRg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+module "Vnet" {
+  source              = "./modules/Vnet"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  VnetName            = var.VnetName
+  subnetName          = var.subnetName
+  nsg                 = var.nsg
+  depends_on          = [azurerm_resource_group.myRg]
+}
+
+module "Vm" {
+  source              = "./modules/vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  subnet_id           = module.Vnet.subnet_id
+  vmPublicIp          = var.vmPublicIp
+  nic                 = var.nic
+  vm                  = var.vm
+  adminUserName       = var.adminUserName
+  depends_on = [azurerm_resource_group.myRg,
+  module.Vnet]
+}
+
+module "storage" {
+  source              = "./modules/storage"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  container           = var.container
+  depends_on          = [azurerm_resource_group.myRg]
+}
+
+module "db" {
+  source               = "./modules/db"
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  database_server_name = var.database_server_name
+  admin_db_username    = var.admin_db_username
+  db_passowrd          = var.db_passowrd
+  start_ip_address     = var.start_ip_address
+  end_ip_address       = var.end_ip_address
+  depends_on           = [azurerm_resource_group.myRg]
+}
